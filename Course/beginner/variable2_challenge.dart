6f26ae9e4642d987a72877a00e45d0fe8d3e5fce
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-13 15:32:17 +0100

    variables

---
+++ Course/beginner/variable2_challenge.dart
@@ -0,0 +1,17 @@
+main(){
+
+  /* create two variables Apple and Banana
+  give them value 15.5 and 16.6 resp.
+  create another var called total cost and add up the two variables.
+  print out"the total cost <the value derived upon adding>"
+   */
+
+  var apple = 15.5;
+  var banana = 16.6;
+  var add = apple + banana;
+
+  var total_cost = add;
+
+  print("the total cost $total_cost");
+
+}
