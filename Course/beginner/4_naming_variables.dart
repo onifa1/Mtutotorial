b8e8f15d5512f2104b2174ba9ab6767c3e53f630
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-13 08:18:34 +0100

    variables

---
+++ Course/beginner/4_naming_variables.dart
@@ -0,0 +1,13 @@
+main(){
+
+  //naming variables
+  /* Rule1: Variable cant begin with a number
+     Rule2: Numbers can be placed after first letter
+     Rule3: No spacing between variables ( you can use _)
+     Rule4: You can use only 2 symbols in variables ($ and _)
+   */
+
+  var name = "Ifeoluwa";
+  print(name);
+
+}
---
+++ Course/beginner/5_const_final.dart
@@ -0,0 +1,17 @@
+main(){
+
+  /*const and final (constant: can not be changed once declared)
+    anything you know wont change always declare it as a constant
+    (Final: exaCT SAME THING NO DIFFERENCE)
+   */
+
+  const pi =3.14;
+  final pi2 =2.14;
+
+  var age = 1988;
+  age = 25;
+
+
+
+
+}
