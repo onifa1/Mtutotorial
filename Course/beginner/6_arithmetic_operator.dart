81a8291f3698e04605d17d0323f7ff1674989beb
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-13 08:33:54 +0100

    variables

---
+++ Course/beginner/6_arithmetic_operator.dart
@@ -0,0 +1,23 @@
+main(){
+
+  //Operators - Arithmetic
+  //(+ - * / %)
+
+  var num1 = 10;
+  var num2 = 5;
+
+  var add = num1 + num2;
+  var sub = num1 - num2;
+  var mult = num1 * num2;
+  var div = num1 / num2;
+
+  var mod = num1 % num2;
+
+  print(div);
+  print(mult);
+  print(add);
+  print(sub);
+  print(mod);
+
+
+}
