d520602ec78a7c6c770c3f2e9393b4dd85862b1c
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-13 16:45:58 +0100

    input

---
+++ Course/beginner/9_input.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+main(){
+
+ //inputs is when you direct something into the system
+
+
+
+}
---
+++ Course/beginner/arithmetic_operator3.dart
@@ -0,0 +1,16 @@
+main(){
+
+  var num1 = 6;
+ // num1 = numi + 1;
+
+  //shortcut
+
+  num1 += 1;
+
+
+  print(num1);
+  
+
+
+
+}
