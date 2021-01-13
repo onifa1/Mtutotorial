57de62b3c4469929468b2f6201f12296db231e31
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-13 17:38:08 +0100

    input

--- Course/beginner/9_input.dart
+++ Course/beginner/9_input.dart
@@ -1,8 +1,17 @@
 import 'dart:io';
 main(){
+    //inputs is when you direct something into the system, they way you receive data from user
+    /* print("what's your name ?");
+  var name = stdin.readLineSync();
+  print("Welcome back user : $name");
+  */
 
- //inputs is when you direct something into the system
+    print("enter in your first number : ");
+    var num1 = int.parse(stdin.readLineSync());
+    print ("enter in your second number :");
+    var num2 = int.parse(stdin.readLineSync());
 
+    var result = num1 + num2;
+    print('the result is : $result');
 
-
-}
+  }
