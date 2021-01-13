bf0f24f119405ff9013b28215bdf3236c1b8d9be
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-13 07:56:06 +0100

    variables

--- Course/beginner/3_variables.dart
+++ Course/beginner/3_variables.dart
@@ -1,5 +1,36 @@
 main() {
 
-  
+  //variables
+  //Numbers we have integars(whole numbers) and double (with decimal point (.))
+  //strings. (Characters)
+  //booleans
+  //Lists
+  //Maps
+  //darts classifacations
+
+  var age = 28;
+  var name = "Ifeoluwa";
+  var is_male = true;
+  var weight = 54.4;
+
+  //how to change variables
+  // once you declare a variable you cant change it again once initialised
+  age = 24;
+  name = "Onifade Ifeoluwa";
+
+  print(name);
+
+  //how to create sentences with variables
+  //Ifeoluwa is 24 years old. use dollar sign $ to show variable, everything in qoute shows as text
+  print("$name is $age years old");
+
+
+
+
+
+
+
+
+
 
 }
---
+++ Course/beginner/variable_challenge.dart
@@ -0,0 +1,10 @@
+main(){
+
+  //create two variables with me and brother and print out "hey i'm <insert name> and my brother is <insert brother name>"
+
+  var me = "Ifeoluwa";
+  var brother = "Feyi";
+
+  print("hey i'm $me and my brother is $brother");
+
+}
