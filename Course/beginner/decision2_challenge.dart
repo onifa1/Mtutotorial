35b71778b66a39f8e519f0c7f8be17511c8634b9
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-19 05:00:02 +0100

    challenge decision

--- Course/beginner/decision2_challenge.dart
+++ Course/beginner/decision2_challenge.dart
@@ -5,8 +5,16 @@ main(){
   the number inputted is positive, negative or 0
 
    */
+  print("Input a number");
+  var sample = int.parse(stdin.readLineSync());
+  if(sample > 0){
+    print("+ve");
+  }else if(sample < 0){
+    print("-ve");
+  }else{
+    print("it's 0");
+  }
 
-  var sample = 10;
 
 
 }
